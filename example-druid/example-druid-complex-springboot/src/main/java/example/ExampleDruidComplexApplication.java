f1f9f5a3491c64a29599b1e8405f085932f974ca
liyinhao <cool2008888@126.com>
2018-07-23 02:50:36 +0800

    defector

--- example-druid/example-druid-complex-springboot/src/main/java/example/ExampleDruidComplexApplication.java
+++ example-druid/example-druid-complex-springboot/src/main/java/example/ExampleDruidComplexApplication.java
@@ -2,13 +2,18 @@ package example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created by liyinhao on 18/7/10.
  */
 @SpringBootApplication
-public class ExampleDruidComplexApplication {
+public class ExampleDruidComplexApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ExampleDruidComplexApplication.class);
+    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ExampleDruidComplexApplication.class).run(args);
